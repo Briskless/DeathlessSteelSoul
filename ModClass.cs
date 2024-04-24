dcf194678ed4db6604c647bdd3e1b6e8ccc47832
Briskless <briskless0@gmail.com>
2024-04-23 22:35:13 -0600

    Set up classes

--- ModClass.cs
+++ ModClass.cs
@@ -7,22 +7,61 @@ using UObject = UnityEngine.Object;
 
 namespace Deathless
 {
-    public class Deathless : Mod
+    public class Deathless : Mod, IMenuMod, IGlobalSettings<GlobalSettings>
     {
         internal static Deathless Instance;
 
-        //public override List<ValueTuple<string, string>> GetPreloadNames()
-        //{
-        //    return new List<ValueTuple<string, string>>
-        //    {
-        //        new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
-        //    };
-        //}
 
-        //public Deathless() : base("Deathless")
-        //{
-        //    Instance = this;
-        //}
+        private bool permadeath;
+
+
+        //Create and initalize a local variable to be able to access the settings
+        public static GlobalSettings GS { get; set; } = new GlobalSettings();
+
+        // First method to implement. The parameter is the read settings from the file
+        public void OnLoadGlobal(GlobalSettings s)
+        {
+            GS = s; // save the read data into local variable;
+        }
+
+        public GlobalSettings OnSaveGlobal()
+        {
+            return GS;//return the local variable so it can be written in the json file
+        }
+
+
+        public bool ToggleButtonInsideMenu => throw new NotImplementedException();
+
+        public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
+        {
+            return new List<IMenuMod.MenuEntry>
+            {
+                new IMenuMod.MenuEntry
+                {
+                    Name = "Permadeath:",
+                    Description = null,
+                    Values = new string[]
+                    {
+                        "Disabled",
+                        "Enabled"
+                    },
+                    Saver = opt => this.permadeath = opt switch
+                    {
+                        0 => false,
+                        1 => true,
+
+                        _ => throw new InvalidOperationException()
+                    },
+                    Loader = () => this.permadeath switch
+                    {
+                        false => 0,
+                        true => 1,
+                    }
+
+                }
+            };
+        }
+
 
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
@@ -31,6 +70,28 @@ namespace Deathless
             Instance = this;
 
             Log("Initialized");
+
+
+            this.permadeath = GS.permadeath;
+
+            On.HeroController.Awake += UpdateGlobalSettings;
+
+            ModHooks.AfterSavegameLoadHook += ConfigurePermadeathSetting;
+
+        }
+
+        private void ConfigurePermadeathSetting(SaveGameData data)
+        {
+            
+        }
+
+        private void UpdateGlobalSettings(On.HeroController.orig_Awake orig, HeroController self)
+        {
+            orig(self);
+
+            Log("Updating Global Settings");
+
+            GS.permadeath = this.permadeath;
         }
     }
 }
